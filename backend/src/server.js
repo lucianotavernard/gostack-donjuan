f7e7e532633f62183edaf30cdf5883d430931620
Luciano Tavernard <lucianotavernard@gmail.com>
2020-04-04 12:51:52 -0300

    refactor: adicionado porta padrão no console do server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 import app from './app'
 
 app.listen(process.env.PORT || 3333, () =>
-  console.log(`server running on port ${process.env.PORT}`)
+  console.log(`Server running on port ${process.env.PORT || 3333}`)
 )
